5266f809581e3485cfe3a3bfd0ea801df780acd8
kdotvs <114116235+kdotvs@users.noreply.github.com>
2025-09-16 23:49:14 -0400

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,1 +1,22 @@
+-- Check data quality (Invalid date order)
 
+select *
+from silver.crm_sales_details
+where sls_order_dt > sls_ship_dt or sls_order_dt > sls_due_dt;
+
+-- check data consistency: betweeen sales, quantity, & price
+-- >> sales = quantity * price
+-- >> Values must not be NULL, Zero or negative.
+
+
+select distinct
+	sls_sales ,
+	sls_quantity ,
+	sls_price
+from silver.crm_sales_details
+where sls_sales != sls_quantity * sls_price
+or sls_sales is null or sls_quantity is null or sls_price is null
+or sls_sales <= 0 or sls_quantity <= 0 or sls_price <= 0
+order by sls_sales, sls_quantity, sls_price;
+
+select * from silver.crm_sales_details;
